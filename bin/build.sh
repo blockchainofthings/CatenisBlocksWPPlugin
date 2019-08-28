75bc009915f2a256831cc46699fdeb432c6ba256
Cláudio Sá de Castro <claudio@hiades.com.br>
2019-08-28 07:17:45 -0300

    Add script to build plugin installation package.

---
+++ bin/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+PLUGIN_VERSION="$(grep "^Version: \+[0-9.]\+" "$SCRIPT_DIR"/../catenis-blocks.php | grep -o "[0-9.]\+")"
+BUILD_DIR="$SCRIPT_DIR/../build/$PLUGIN_VERSION"
+
+if [ -f "$BUILD_DIR"/catenis-blocks.zip ]; then
+    echo "Plugin version ($PLUGIN_VERSION) already built"
+    exit -1
+fi
+
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir "$BUILD_DIR"
+fi
+
+cd "$SCRIPT_DIR"/..
+zip -r "$BUILD_DIR"/catenis-blocks.zip ./* -x ./.\* \*.DS_Store ./\*.code-workspace ./composer.\* ./svn/\* ./vendor/\* ./build/\* ./bin/\* ./node_modules/\*
