e5c6ab6554265bf61f24df69a34e87135b5df37f
Cláudio Sá de Castro <claudio@hiades.com.br>
2020-01-24 16:18:25 -0300

    Update copyright notice.

--- catenis-blocks.php
+++ catenis-blocks.php
@@ -9,7 +9,7 @@ Author URI: https://blockchainofthings.com
 Text Domain: catenis-blocks
 License: MIT
 
-Copyright 2019 Blockchain of Things Inc.
+Copyright 2020 Blockchain of Things Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
